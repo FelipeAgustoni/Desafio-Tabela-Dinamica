392d02e9537372a5b70a9225201c29a669a5ac76
Felipe Agustoni <felipe_trabalhoo@hotmail.com>
2017-05-15 14:31:00 -0300

    atualizando projeto tb dinamica

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,4 @@
 //Executa quando o documento é carregado
-
 var pessoas = []
 var id = 0;
 
@@ -11,56 +10,118 @@ $(document).ready(function () {
 		$('#txtRg').mask('99.999.999-9');
 
         $("#btnAdicionar").click(function(){
-			//Validando campos obrigatorios
-        	if (!$('#txtNome').val() || !$('#selSexo').val() || !$('#txtDataNascimento').val() || !$('#txtCpf').val()) {
-        		alert('Campo(s) obrigatorio(s) nao preenchido');
-        	} else {            
-	            pessoas.push({
+
+        	// Atribuindo o valor do input id hidden em idPessoa
+        	idPessoa = $("#id").val().trim();
+
+        	//Validando campos obrigatorios
+        	if (!$('#txtNome').val().trim() || !$('#selSexo').val().trim() || !$('#txtDataNascimento').val().trim() || !$('#txtCpf').val().trim()) {
+        		alert('Campo(s) obrigatorio(s) não preenchido(s) !');
+        		clear();
+        	} else {
+
+        	// Verificando se tem id ou nao, para saber se é alterar ou salvar
+    		if (idPessoa != ""){
+
+    			for (var i = 0; i < pessoas.length; i++) {
+				pessoa = pessoas[i];
+
+					if(pessoa.id == idPessoa){
+	    				pessoa.nome = $("#txtNome").val();
+	    				pessoa.sexo = $("#selSexo").val();
+	    				pessoa.data = $("#txtDataNascimento").val();
+	    				pessoa.cpf = $("#txtCpf").val();
+	    				pessoa.rg = $("#txtRg").val();
+	    			}
+    			}
+    		}
+
+			else {            
+	            pessoas.push({ // Insere no vetor
 	            	id: ++id,
 	            	nome: $("#txtNome").val(),
 	            	sexo: $("#selSexo").val(),
 	            	data: $("#txtDataNascimento").val(),
 	            	cpf: $("#txtCpf").val(),
 	            	rg: $("#txtRg").val()
-	            });
-
-	            carregarTabela();
+	        	});
         	}
+        }
+
+        	clear();
+
+    		carregarTabela();
 
         });
 
+
+    update = function(id){
+		for (var i = 0; i < pessoas.length; i++) {
+			pessoa = pessoas[i];
+			if(pessoa.id == id){
+				$("#id").val(pessoa.id);
+				$("#txtNome").val(pessoa.nome);
+				$("#selSexo").val(pessoa.sexo);
+				$("#txtDataNascimento").val(pessoa.data);
+				$("#txtCpf").val(pessoa.cpf);
+				$("#txtRg").val(pessoa.rg);
+			}
+		}
+	}
+
+        //funcao para limpar os campos
+        function clear(){
+        	$("#id").val("");
+			$('#txtNome').val("");
+		    $('#selSexo').val("");
+		    $('#txtDataNascimento').val("");
+		    $('#txtCpf').val("");
+		    $('#txtRg').val("");
+        }
+
     // Removendo um registro da tabela
     remover = function(id) {
+
 	   	var confirma = confirm("Deseja realmente excluir este registro ? O Processo não poderá ser revertido !");
 	    
 	    if(confirma){
+	    	// Percorre o vetor, comparando se o id que recebeu como parametro na funcao
+			// é igual ao id do botao em que o usuario clicou na tr da tabela
 	    	for (var i = 0; i < pessoas.length; i++) {
 	    		pessoa = pessoas[i];
 	    		if(pessoa.id == id){
+	    			// Splice espera 2 parametros, indice - qtd
+	    			// remove aquele elemento, no indice i, 1 registro
 	    			pessoas.splice(i, 1);
 	    		}
 	    	}
+
+	    	clear();
+
 	    	carregarTabela();
 	    }
     }
 
 });
 
+//prepara a tabela
 function carregarTabela () {
-	$("#tbodyTable").html('');
 
+	$("#tbodyTable").html(''); // limpa a tabela
 	for (var i = 0; i < pessoas.length; i++) {
 		var pessoa = pessoas[i];
+
+		// analogamente, String Builder do Java
 		var html = `
     		<tr>
     			<td>${pessoa.nome}</td>
-    			<td>${pessoa.sexo}</td>
+    			<td>${pessoa.sexo == "M" ? "Masculino" : "Feminino"}</td>
     			<td>${pessoa.data}</td>
     			<td>${pessoa.cpf}</td>
     			<td>${pessoa.rg}</td>
 
     			<td>
-        			<button type="button" class="btn btn-success">
+        			<button type="button" class="btn btn-success" onclick="update(${pessoa.id})">
         			<span class="glyphicon glyphicon-pencil">
         			</span>
         			</button>
@@ -75,14 +136,16 @@ function carregarTabela () {
     		</tr>
     	`;
 
+    	//Insere na tabela 
 		$("#tbodyTable").append(html);
 	}
 
+	// Contador para mostrar o total de Registros de acordo com o length do vetor
 	$('#totalReg').text(pessoas.length);
 }
 
 /*
-//Atualizando o "Total de Registros: " da tabela
+Atualizando o "Total de Registros: " da tabela
             $("#tbodyTable").append(html);
             $('#totalReg').text($('#tbodyTable > tr').length); //document.getElementById('tbodyTable').rows.length);
 */
